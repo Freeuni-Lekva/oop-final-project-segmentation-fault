7391dfbb5729cbabba7a8113e8d16dbed99c904a
TengizGvelesiani <tgvel22@freeuni.edu.ge>
2025-06-14 22:19:56 +0400

    Created BookKeeper entity and DTO

--- LibraryProject/src/main/java/com/example/libraryproject/model/entity/BookKeeper.java
+++ LibraryProject/src/main/java/com/example/libraryproject/model/entity/BookKeeper.java
@@ -1,4 +1,17 @@
 package com.example.libraryproject.model.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class BookKeeper {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
 }
