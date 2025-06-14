6dff9bd9ca74a2861f4bb5ff94081afdba2344bc
nikaduri <nikadurishvili3@gmail.com>
2025-06-14 21:33:01 +0400

    cleared book

--- LibraryProject/src/main/java/com/example/libraryproject/model/entity/Book.java
+++ LibraryProject/src/main/java/com/example/libraryproject/model/entity/Book.java
@@ -1,21 +1,5 @@
 package com.example.libraryproject.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-
-@Entity
 public class Book {
 
-    @Id
-    private Long id;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
