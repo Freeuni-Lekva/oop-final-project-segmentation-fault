2aba118224ff6e391343b211a23e3050e46cda69
CarlTeapot <arevadzemate@gmail.com>
2025-06-14 14:33:36 +0400

    Library Management:   * implement Database connection

--- LibraryProject/src/main/java/com/example/libraryproject/servlet/ApplicationContextListener.java
+++ LibraryProject/src/main/java/com/example/libraryproject/servlet/ApplicationContextListener.java
@@ -1,6 +1,30 @@
 package com.example.libraryproject.servlet;
 
+import com.example.libraryproject.configuration.DBConnectionConfig;
+import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+@WebListener
 public class ApplicationContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+        try {
+            Session session = DBConnectionConfig.getSessionFactory().openSession();
+
+            System.out.println("✅ Hibernate schema created or validated successfully.");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("❌ Failed to initialize Hibernate schema", e);
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        DBConnectionConfig.getSessionFactory().close();
+    }
 }
