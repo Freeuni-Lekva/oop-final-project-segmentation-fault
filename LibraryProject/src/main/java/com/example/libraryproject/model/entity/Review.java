7c18f9ed25e21324ea3057b3f54ffa5b00998625
rezibechvaia <rbech22@freeuni.edu.ge>
2025-06-14 22:25:16 +0400

    Review Entity

--- LibraryProject/src/main/java/com/example/libraryproject/model/entity/Review.java
+++ LibraryProject/src/main/java/com/example/libraryproject/model/entity/Review.java
@@ -1,4 +1,24 @@
 package com.example.libraryproject.model.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "rating", nullable = false)
+    private int rating;
+
+    @Column(name = "comment", length = 1000)
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Book book;
 }
