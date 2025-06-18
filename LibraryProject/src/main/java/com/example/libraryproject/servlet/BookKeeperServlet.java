16a16f0d10224bae2d125d82da11cbd68691347d
EleneJobava <ejoba22@freeuni.edu.ge>
2025-06-18 20:04:11 +0400

    Added BookKeeperServlet

---
+++ LibraryProject/src/main/java/com/example/libraryproject/servlet/BookKeeperServlet.java
@@ -0,0 +1,106 @@
+package com.example.libraryproject.servlet;
+
+import com.example.libraryproject.configuration.ApplicationProperties;
+import com.example.libraryproject.model.dto.BookAdditionRequest;
+import com.example.libraryproject.model.entity.Order;
+import com.example.libraryproject.service.BookKeeperService;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "BookKeeperServlet", urlPatterns = "/api/bookkeeper/*")
+public class BookKeeperServlet extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        BookKeeperService bookKeeperService = (BookKeeperService) req.getServletContext()
+                .getAttribute(ApplicationProperties.BOOKKEEPER_SERVICE_ATTRIBUTE_NAME);
+
+        String path = req.getPathInfo();
+
+        switch(path) {
+            case "/add-book":
+                handleAddBook(req, bookKeeperService);
+                break;
+
+            case "/mark-borrowed":
+                handleMarkBorrowed(req, bookKeeperService);
+                break;
+
+            case "/ban-user":
+                handleBanUser(req, bookKeeperService);
+                break;
+
+            default:
+                resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {}
+
+    @Override
+    public void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        BookKeeperService bookKeeperService = (BookKeeperService) req.getServletContext()
+                .getAttribute(ApplicationProperties.BOOKKEEPER_SERVICE_ATTRIBUTE_NAME);
+
+        String path = req.getPathInfo();
+
+        switch(path) {
+            case "/delete-book":
+                handleDeleteBook(req, bookKeeperService);
+                break;
+
+            default:
+                resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+
+    private void handleAddBook(HttpServletRequest req, BookKeeperService bookKeeperService) throws IOException {
+        String title = req.getParameter("title");
+        String author = req.getParameter("author");
+        String description = req.getParameter("description");
+        String genre = req.getParameter("genre");
+
+        if(title == null || author == null){
+            throw new IllegalArgumentException("Title and author are required to add a new book.");
+        }
+
+        BookAdditionRequest bookAdditionRequest = new BookAdditionRequest(title, author, description, genre);
+        bookKeeperService.addBook(bookAdditionRequest);
+    }
+
+    private void handleMarkBorrowed(HttpServletRequest req, BookKeeperService bookKeeperService) {
+        String orderPublicId = req.getParameter("orderPublicId");
+        if(orderPublicId == null){
+            throw new IllegalArgumentException("Order Public ID is required to mark book as borrowed.");
+        }
+
+        bookKeeperService.tookBook(orderPublicId);
+    }
+
+    private void handleDeleteBook(HttpServletRequest req, BookKeeperService bookKeeperService) {
+        String bookPublicId = req.getParameter("bookPublicId");
+        if(bookPublicId == null){
+            throw new IllegalArgumentException("Order Public ID is required to add a new book.");
+        }
+
+        bookKeeperService.deleteBook(bookPublicId);
+    }
+
+    private void handleBanUser(HttpServletRequest req, BookKeeperService bookKeeperService) {
+        String orderIdStr = req.getParameter("orderId");
+
+        if(orderIdStr == null){
+            throw new IllegalArgumentException("Order ID is required to ban a user.");
+        }
+
+        Long orderId = Long.parseLong(orderIdStr);
+        Order order= new Order();
+        order.setId(orderId);
+        bookKeeperService.banUser(order);
+    }
+
+}
